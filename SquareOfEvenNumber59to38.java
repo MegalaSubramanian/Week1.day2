64282fd11a65e6c4fd0a789b764d950242ecfff7
MegalaSubramanian <98220722+MegalaSubramanian@users.noreply.github.com>
2022-01-23 17:27:52 +0530

    Add files via upload

---
+++ SquareOfEvenNumber59to38.java
@@ -0,0 +1,16 @@
+package week1.day2.Assessment;
+
+public class SquareOfEvenNumber59to38 {
+
+	public static void main(String[] args) {
+
+		for (int i = 59; i >= 38; i--) {
+			if(i%2==0) {
+			int j = i * i;
+			System.out.println("Square of the value "+i+" is "+j);
+		}
+		}
+
+	}
+
+}
---
+++ SumOfnumberRange.java
@@ -0,0 +1,14 @@
+package week1.day2.Assessment;
+
+public class SumOfnumberRange {
+
+	public static void main(String[] args) {
+		int range = 5;
+		int result = 0;
+		for (int i = 1; i <= range; i++) {
+			result = result + i;
+		}
+		System.out.println("The Sum of the Range 1 to 5 = " + result);
+	}
+
+}
